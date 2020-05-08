69d44ceca893b49562e96fe30b53c108cde3491d
Charles P Oliver <charles@cocoa.consulting>
2020-05-08 17:26:00 +0100

    disable gatsby-source-filesystem config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,13 @@ module.exports = {
   plugins: [
     `gatsby-plugin-theme-ui`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
