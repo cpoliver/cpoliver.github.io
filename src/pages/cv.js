0f22b31576b770e855db1948ccf147fbb4272499
Charles P Oliver <charles@cocoa.consulting>
2020-05-10 13:52:03 +0100

    add profile component to cv

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -13,7 +13,16 @@ const Header = () => (
       textAlign: "center",
     }}
   >
-    <Text as="h1" sx={{ fontWeight: "bold", pt: 6, pb: 1, fontSize: 46 }}>
+    <Text
+      as="h1"
+      sx={{
+        fontSize: 46,
+        fontWeight: "bold",
+        opacity: 0.8,
+        pb: 1,
+        pt: 6,
+      }}
+    >
       {cv.name.first}&nbsp;
       <Text as="span" fontWeight="normal">
         {cv.name.initial}&nbsp;
@@ -24,7 +33,6 @@ const Header = () => (
     <Flex
       sx={{
         alignSelf: "stretch",
-        backgroundColor: "#eee",
         flex: 1,
         mt: 4,
       }}
@@ -60,12 +68,72 @@ const Header = () => (
   </Flex>
 )
 
-const Experience = props => <Box {...props} />
+const Experience = ({ years, skills, sx = {} }) => (
+  <Flex
+    flex={1}
+    mt={6}
+    px={4}
+    sx={{
+      borderColor: "muted",
+      borderStyle: "solid",
+      borderWidth: "0",
+      alignItems: "center",
+      ...sx,
+    }}
+  >
+    <Text
+      sx={{
+        flex: 1,
+        fontWeight: "bold",
+        mr: 2,
+        py: 2,
+        textAlign: "right",
+      }}
+    >
+      {years}
+      <br />
+      years
+    </Text>
+    <Text
+      sx={{
+        flex: 3,
+        ml: 2,
+      }}
+    >
+      {skills.join("\n")}
+    </Text>
+  </Flex>
+)
 
 const Profile = () => (
-  <Flex flexDirection="column">
-    {/*  */}
-    Profile
+  <Flex
+    sx={{
+      alignItems: "center",
+      flexDirection: "column",
+      margin: "0 auto",
+      maxWidth: 800,
+      py: 8,
+    }}
+  >
+    <Text
+      sx={{
+        flex: 1,
+        lineHeight: "150%",
+      }}
+    >
+      {cv.statement.join(" ")}
+    </Text>
+    <Flex flex={1} alignSelf="stretch">
+      <Experience years="10+" skills={["C# .net", "Agile Delivery"]} />
+      <Experience
+        years="5+"
+        skills={["React", "NodeJS"]}
+        sx={{
+          borderWidth: "0 1px",
+        }}
+      />
+      <Experience years="3+" skills={["React Native", "Remote Working"]} />
+    </Flex>
   </Flex>
 )
 
